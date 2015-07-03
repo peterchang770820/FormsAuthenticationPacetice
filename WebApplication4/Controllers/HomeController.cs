e450ddf761570e14d4b3297ad093a4f8935ccbcd
Peter Chang <peter@miniasp.com>
2015-07-03 16:42:13 +0800

    init

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Authorize] 
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// 呈現後台使用者登入頁
+        /// </summary>
+        /// <param name="ReturnUrl">使用者原本Request的Url</param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        public ActionResult Login(string ReturnUrl)
+        {
+            //ReturnUrl字串是使用者在未登入情況下要求的的Url
+            LoginVM vm = new LoginVM() { ReturnUrl = ReturnUrl };
+            return View(vm);
+        }
+
+        /// <summary>
+        /// 後台使用者進行登入
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <param name="u">使用者原本Request的Url</param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginVM vm)
+        {
+
+            //沒通過Model驗證(必填欄位沒填，DB無此帳密)
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+
+
+            //都成功...
+            //進行表單登入 ※之後User.Identity.Name的值就是vm.Account帳號的值
+            //導向預設Url(Web.config裡的defaultUrl定義)或使用者原先Request的Url
+            FormsAuthentication.RedirectFromLoginPage(vm.Account, false);
+
+
+
+
+
+            //剛剛已導向，此行不會執行到
+            return Redirect(FormsAuthentication.GetRedirectUrl(vm.Account, false));
+
+
+        }
+        /// <summary>
+        /// 後台使用者登出動作
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            //清除Session中的資料
+            Session.Abandon();
+            //登出表單驗證
+            FormsAuthentication.SignOut();
+            //導至登入頁
+            return RedirectToAction("Login", "Home");
+        }
+ 
+
+
+    }
+}
---
+++ WebApplication4/Models/LoginVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class LoginVM : IValidatableObject
+    {
+
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Required]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Required]
+        public string Password { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //將使用者輸入的字串轉成Base64String
+            //string base64Password = Convert.ToBase64String(Encoding.UTF8.GetBytes(Password));
+            //todo到DB抓使用者資料
+            //假如抓不到系統使用者資料
+            //※為了Demo用這種寫法，實際請換成判斷DB的資料存不存在
+            if (!(Account == "123" && Password == "123"))
+            {
+                yield return new ValidationResult("無此帳號或密碼", new string[] { "Account" });
+            }
+        }
+    }
+}
